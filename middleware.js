9e77fb636b5f53e1b910a8bdc7fbba661f94033e
manishmadeshia06 <162539437+manishmadeshia06@users.noreply.github.com>
2024-07-12 01:10:31 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -19,15 +19,7 @@ module.exports.saveRedirectUrl = (req,res,next) =>{
     next();
 }
 
-// module.exports.isOwner = async(req,res,next)=>{
-//     let {id} = req.params;
-//     let listing = await Listing.findById(id);
-//     if(!listing.owner._id.equals(res.locals.currUser._id)){
-//         req.flash("error", "You are not the owner of this listing");
-//         return res.redirect(`/listings/${id}`);
-//     }
-//     next();
-// }
+
 
 module.exports.isOwner = async (req, res, next) => {
     try {
